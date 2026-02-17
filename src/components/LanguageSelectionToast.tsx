584f14b423c6fb1d4ffc778088c0da633e29858e
Alexander Tarasov <lexeor.ross@gmail.com>
2026-02-17 10:41:06 +0100

    update SEO

--- src/components/LanguageSelectionToast.tsx
+++ src/components/LanguageSelectionToast.tsx
@@ -8,7 +8,8 @@ const LanguageSelectionToast: FC = () => {
     <div
       className="relative min-h-56 bg-green-900/30 p-6 rounded-2xl font-light shadow-xl flex flex-col items-center justify-between gap-4 min-w-[300px] backdrop-blur-sm animate-in fade-in slide-in-from-bottom-4 duration-500 overflow-hidden">
       <LanguagesIcon size={164} strokeWidth={2.5} className="absolute -top-2 -left-2 opacity-3" />
-      <Multilingual as="h3" translationKey="selectLanguage" className="text-base leading-tight align-text-top" />
+      <Multilingual as="h3" translationKey="selectLanguage"
+                    className="text-base md:text-lg leading-tight align-text-top " />
       <LanguageSwitcher />
     </div>
   );
